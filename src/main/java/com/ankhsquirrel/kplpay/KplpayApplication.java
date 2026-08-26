aa548fc2c35c01d9302f7ab3d8960a16378e7e5c
AnkhSquirrel <ankhsquirrel@pm.me>
2026-08-26 18:44:42 +0200

    Initial commit

---
+++ src/main/java/com/ankhsquirrel/kplpay/KplpayApplication.java
@@ -0,0 +1,13 @@
+package com.ankhsquirrel.kplpay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KplpayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KplpayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ankhsquirrel/kplpay/KplpayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ankhsquirrel.kplpay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KplpayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
